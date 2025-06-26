45d8812cb98ba512d7901217bf288820a2d71a02
Nimna Pathum <113501913+nimnapathum@users.noreply.github.com>
2025-06-26 11:07:47 +0530

    create verify token file

--- middleware/verifyToken.ts
+++ middleware/verifyToken.ts
@@ -2,9 +2,12 @@
 import { Request, Response, NextFunction } from "express";
 import admin from "../firebaseAdmin";
 
-export const verifyToken = async (req: Request, res: Response, next: NextFunction) => {
+export const verifyToken = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   const token = req.headers.authorization?.split(" ")[1];
-  if (!token) return res.status(401).send("Unauthorized");
+  if (!token) {
+    res.status(401).send("Unauthorized");
+    return;
+  }
 
   try {
     const decodedToken = await admin.auth().verifyIdToken(token);
