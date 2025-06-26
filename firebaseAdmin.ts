6b98c9fecaa32af84233e55f828103c030395c67
Nimna Pathum <113501913+nimnapathum@users.noreply.github.com>
2025-06-26 11:56:30 +0530

    create firebase admin file

---
+++ firebaseAdmin.ts
@@ -0,0 +1,9 @@
+// firebaseAdmin.ts
+import admin from "firebase-admin";
+import serviceAccount from "./serviceAccountKey.json";
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+export default admin;
