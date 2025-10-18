31b54117f1454101e10c84211f48def2d47e91b6
irumi <irumiabeywickrama@gmail.com>
2025-10-18 13:44:54 +0530

    [ADD] Integrate mentor-mentee connection routes into the application

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import mentorApplicationRoutes from "./routes/mentorApplication.routes";
 import mentorProfileRoutes from "./routes/mentorProfile.routes";
 import menteeApplicationRoutes from "./routes/menteeApplication.routes";
+import mentorMenteeConnectionRoutes from "./routes/mentorMenteeConnection.routes";
 import influencerApplicationRoutes from "./routes/influencerApplication.routes";
 import guideApplicationRoutes from "./routes/guideApplication.routes";
 import subscriptionRoutes from "./routes/subscription.routes";
@@ -85,6 +86,7 @@ app.use("/api/user", profileRoutes);
 // Application APIs
 app.use("/api/mentor-applications", mentorApplicationRoutes);
 app.use("/api/mentee-applications", menteeApplicationRoutes);
+app.use("/api/mentor-mentee-connections", mentorMenteeConnectionRoutes);
 app.use("/api/mentor", mentorProfileRoutes);
 app.use("/api/influencer-applications", influencerApplicationRoutes);
 app.use("/api/guide-applications", guideApplicationRoutes);
